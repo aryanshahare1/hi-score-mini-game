a8e49fed40418297f602f1ee60033406de3c5d75
aryanshahare1 <aryanshahare05@gmail.com>
2024-09-09 12:38:23 +0530

    Added hi_score

---
+++ hi_score.py
@@ -0,0 +1,24 @@
+import random 
+
+def game():
+    print("you are playing the game..")
+    score = random.randint(1,69)
+
+    # fetch hiscore
+
+    with open ("hiscore.txt") as f:
+        hiscore = f.read()
+
+        if (hiscore!=""):
+            hiscore=int(hiscore)
+
+        else:
+            hiscore = 0
+
+    print (f"your score : {score}")
+    if (score>hiscore):
+        with open("hiscore.txt" ,"w") as f:
+            f.write(str(score))
+    return score
+
+game()
